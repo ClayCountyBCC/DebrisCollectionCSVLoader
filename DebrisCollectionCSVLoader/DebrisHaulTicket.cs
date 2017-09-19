c17f76ee8e5742dbce53026134d996ce4014b448
Jeremy West <jeremy.west@claycountygov.com>
2017-09-19 13:37:59 -0400

    Add project files.

---
+++ DebrisCollectionCSVLoader/DebrisHaulTicket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DebrisCollectionCSVLoader
+{
+  class DebrisHaulTicket
+  {
+    public static string HaulTicketBarcode { get; set; } = "" ;
+    public static string TruckId { get; set; } = "";
+    public static string DebrisType { get; set; } = "";
+    public static string Latitude { get; set; } = "";
+    public static string Longitude { get; set; } = "";
+    public static string Sector { get; set; } = "";
+    public static string Zone { get; set; } = "";
+    public static string PropertyType { get; set; } = "";
+    public static string CreatedBy { get; set; } = "";
+    public static string CreateDate { get; set; } = "";
+
+    public DebrisHaulTicket()
+    {
+
+    }
+
+
+    public static DebrisHaulTicket FromCsv(string csvLine)
+    {
+      string[] values = csvLine.Split(',');
+      DebrisHaulTicket debrisHaulTicket = new DebrisHaulTicket();
+      DebrisHaulTicket.HaulTicketBarcode = Convert.ToString(values[4]);
+      DebrisHaulTicket.TruckId =  Convert.ToString(values[7]);
+      DebrisHaulTicket.DebrisType =  Convert.ToString(values[5]);
+      DebrisHaulTicket.Latitude = Convert.ToString(values[0]);
+      DebrisHaulTicket.Longitude = Convert.ToString(values[1]);
+      DebrisHaulTicket.Sector = Convert.ToString(values[8]);
+      DebrisHaulTicket.Zone = Convert.ToString(values[9]);
+      DebrisHaulTicket.PropertyType = Convert.ToString(values[6]);
+      DebrisHaulTicket.CreatedBy = Convert.ToString(values[2]);
+      DebrisHaulTicket.CreateDate = Convert.ToString(values[3]);
+
+      return debrisHaulTicket;
+
+    }
+
+
+  }
+}
---
+++ DebrisCollectionCSVLoader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebrisCollectionCSVLoader
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<DebrisHaulTicket> values = File.ReadAllLines(@"C:\Users\westje\Documents\Visual Studio 2017\Projects\DebrisCollectionCSVLoader\DebrisCollectionCSVLoader\Debris_Haul_Teckets_Test.csv")
+                               .Skip(1)
+                               .Select(line => DebrisHaulTicket.FromCsv(line))
+                               .ToList();
+
+      for (int i = 0; i < values.Count - 1; i++)
+        Console.WriteLine("value: " + values[i].ToString());
+
+      Console.ReadKey();
+    }
+
+
+
+  }
+}
